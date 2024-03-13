5e4704d4863b99556e8e788b04ed8f6996e45114
Xqyat <romalev2006@mail.ru>
2024-03-13 08:23:16 +0300

    Modified Pz_13 and report Pz_13

--- Pz_13/Pz_13.1.py
+++ Pz_13/Pz_13.1.py
@@ -3,14 +3,12 @@
 import random as rand
 Rows_vert = rand.randint(2, 4)
 Rows_hor = rand.randint(2, 4)
-Matrix = [[rand.randint(1, 8) for j in range(Rows_hor)] for i in range(Rows_vert)]
-for i in Matrix:
-    print(i)
+Matrix = [[rand.randint(1, 8) for i in range(Rows_hor)] for i in range(Rows_vert)]
+[print(row) for row in Matrix]
 N_num = int(input("Введите номер столбца N (отсчет с нуля): "))
-sum_col = 0
+sum_col = sum(Matrix[i][N_num] for i in range(Rows_vert))
 subtr_col = 1
 for i in range(Rows_vert):
-    sum_col += Matrix[i][N_num]
     subtr_col *= Matrix[i][N_num]
-print(f"\nСумма элементов столбца N: {sum_col}")
+print(f"Сумма элементов столбца N: {sum_col}")
 print(f"Произведение элементов столбца N: {subtr_col}")
